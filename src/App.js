b3283430ba80b17c9bec89d386a9273c6f147c0b
Artemy Brunevich <brunewitch@mail.ru>
2023-11-23 01:51:47 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './navbar'
+import Footer from './footer'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      
+    </div>
+  );
+}
+
+export default App;
